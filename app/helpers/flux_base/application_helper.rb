c3428ba0e217d1a595da359d6b57244f7d35f1a7
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-02-12 12:54:18 -0800

    Add useful Bulma helpers, remove gems that don't work in practice

--- app/helpers/flux_base/application_helper.rb
+++ app/helpers/flux_base/application_helper.rb
@@ -1,4 +1,17 @@
 module FluxBase
   module ApplicationHelper
+    def application_includes
+      styles = stylesheet_link_tag('application', media: 'all', 'data-turbolinks-track': 'reload')
+      js = javascript_include_tag('application', 'data-turbolinks-track': 'reload')
+      styles.concat(js)
+    end
+
+    def controller_specific_includes
+      unless controller_name == 'application'
+        styles = stylesheet_link_tag params[:controller], 'data-turbolinks-track': 'reload'
+        js = javascript_include_tag params[:controller], 'data-turbolinks-track': 'reload'
+        styles.concat(js)
+      end
+    end
   end
 end
---
+++ app/helpers/flux_base/bulma_helpers.rb
@@ -0,0 +1,59 @@
+module FluxBase
+  module BulmaHelpers
+    # Add controls in the block passed to this method.
+    def actions_fieldset(title = '')
+      field_set_tag(title) do
+        content_tag(:div, class: 'field has-addons') do
+          yield
+        end
+      end.concat(tag(:hr))
+    end
+
+    def display_notice
+      return unless notice.present?
+      content_tag(:div, class: 'notification is-info') do
+        notice
+      end
+    end
+
+    def display_alert
+      return unless alert.present?
+      content_tag(:div, class: 'notification is-danger') do
+        alert
+      end
+    end
+
+    # Generates pretty printed JSON for obj.
+    def display_json(obj)
+      JSON.pretty_generate(obj)
+    rescue JSON::GeneratorError
+      JSON.pretty_generate(JSON.parse(obj.to_json))
+    end
+
+    # Replaces the auto-generated errors div for record forms.
+    def display_errors(record)
+      return unless record.errors.any?
+
+      title = "#{pluralize(record.errors.count, 'error')} prohibited this #{record.class.name.downcase} from being saved:"
+      create_message(title, class: 'is-danger') do
+        content_tag(:ul) do
+          record.errors.full_messages.map do |msg|
+            content_tag(:li, msg)
+          end.inject(&:concat)
+        end
+      end
+    end
+
+    # Creates a Bulma message with the given title and options. Use a block to define the card body.
+    def create_message(title, options = {})
+      content_tag(:article, class: 'message ' + options.delete(:class), **options) do
+        header = content_tag(:div, title, class: 'message-header')
+        body = content_tag(:div, class: 'message-body') do
+          yield
+        end
+
+        header.concat(body)
+      end
+    end
+  end
+end
