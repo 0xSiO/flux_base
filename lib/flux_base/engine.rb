29f14d8898ed8c022c67b104ab5d66918ea4885c
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-11-11 17:40:29 -0800

    Add back any needed requires

--- lib/flux_base/engine.rb
+++ lib/flux_base/engine.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+require 'jbuilder'
+require 'rails_semantic_logger'
+require 'slim-rails'
+require 'turbolinks'
+require 'webpacker'
+
+if Rails.env == 'development' || Rails.env == 'test'
+  require 'byebug'
+  require 'database_cleaner'
+
+  if Rails.env == 'development'
+    require 'spring/watcher/listen'
+    require 'web_console'
+  else
+    require 'capybara/rails'
+  end
+end
+
 module FluxBase
   class Engine < ::Rails::Engine
     isolate_namespace FluxBase
