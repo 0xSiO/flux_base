e26fcd06f2da0cb8e7c03c9449ffc67bcce07a6f
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-11-11 20:29:48 -0800

    Add in application_includes with correct js imports

--- app/helpers/flux_base/application_helper.rb
+++ app/helpers/flux_base/application_helper.rb
@@ -2,6 +2,12 @@
 
 module FluxBase
   module ApplicationHelper
+    def application_includes
+      styles = stylesheet_link_tag('application', media: 'all', 'data-turbolinks-track': 'reload')
+      js = javascript_pack_tag('application', 'data-turbolinks-track': 'reload')
+      styles.concat(js)
+    end
+
     # Generates pretty printed JSON for obj.
     def display_json(obj)
       JSON.pretty_generate(obj)
